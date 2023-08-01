ac08aa28b2ca2bcef9a14e4a87541aba77a45108
Michal Piotrowski <asyncfn.com@gmail.com>
2023-08-01 12:25:31 +0000

    #2 template service p1
     #2 template service p2
    
     #2 template service p3
    
     #2 template service p4
    
     #2 template service p5
    
     #2 template service p6
    
     #2 template service p7
    
     #2 template service p8
    
     #2 template service p9
    
     #2 template service p10
    
     #2 template service p11
    
     #2 template service p12
    
     #2 template service p13
    
     #2 template service p14
    
     #2 template service p15
    
     #2 template service p16

--- octopus_function_template.py
+++ octopus_function_template.py
@@ -1,7 +1,139 @@
-from flask import Flask
+import base64, os, uuid
+from datetime import datetime, timedelta
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+results = {}
+
+@app.route("/v1/function-bar-async/setup", methods=["POST"])
+def function_bar_async_setup():
+    data = request.json
+    force_setup = data.get("force_setup", False)
+    file = "model.dat"
+
+    if not os.path.isfile(file) or force_setup:
+        f = open(file, "a")
+        f.write("Lets create some model file")
+        f.close()
+
+        return {
+            "setup": "performed"
+        }, 201
+
+    return {
+        "setup": "not_performed"
+    }, 201
+
+@app.route("/v1/function-bar-async", methods=["POST"])
+def function_bar_async():
+    data = request.json
+    value1 = data.get("value1", "")
+    value2 = data.get("value2", "")
+
+    id = str(uuid.uuid4())
+    status = "initial"
+    response_text = "Some async response text " + value1 + " " + value2
+    estimated_response_at = str(datetime.now() + timedelta(seconds=5))
+
+    response = {
+        "id": id,
+        "estimated_response_at": estimated_response_at,
+        "progress": 0,
+        "status": status,
+        "response": response_text,
+        "file_attachements": list()
+    }
+
+    results[id] = response
+
+    return jsonify(response), 201
+
+@app.route("/v1/function-bar-async/<string:id>", methods=["GET"])
+def function_bar_async_status(id):
+    response = results[id]
+
+    estimated_response_at = str(datetime.now() + timedelta(seconds=5))
+
+    if response["progress"] < 25:
+        response["estimated_response_at"] = estimated_response_at
+        response["progress"] = 25
+        response["status"] = "processing"
+    elif response["progress"] < 50:
+        response["estimated_response_at"] = estimated_response_at
+        response["progress"] = 50
+    elif response["progress"] < 75:
+        response["estimated_response_at"] = estimated_response_at
+        response["progress"] = 75
+    elif response["progress"] < 100:
+        response["estimated_response_at"] = estimated_response_at
+        response["progress"] = 100
+        response["status"] = "processed"
+        content = str(base64.b64encode(b"test of generated content"))
+        file_attachement = {
+            "content": content,
+            "file_name": "test.txt",
+            "media_type": "text/plain",
+        }
+        file_attachements = list()
+        file_attachements.append(file_attachement)
+        response["file_attachements"] = file_attachements
+
+    results[id] = response
+
+    return jsonify(response), 201
+
+@app.route("/v1/function-foo-sync/setup", methods=["POST"])
+def function_foo_sync_setup():
+    data = request.json
+    force_setup = data.get("force_setup", False)
+    file = "model.dat"
+
+    if not os.path.isfile(file) or force_setup:
+        f = open(file, "a")
+        f.write("Lets create some model file")
+        f.close()
+
+        return {
+            "setup": "performed"
+        }, 201
+
+    return {
+        "setup": "not_performed"
+    }, 201
+
+@app.route("/v1/function-foo-sync", methods=["POST"])
+def function_foo_sync():
+    data = request.json
+    value1 = data.get("value1", "")
+    value2 = data.get("value2", "")
+
+    id = str(uuid.uuid4())
+    status = "processed"
+    response_text = "Some sync response text " + value1 + " " + value2
+    estimated_response_at = str(datetime.now() + timedelta(seconds=5))
+
+    response = {
+        "id": id,
+        "estimated_response_at": estimated_response_at,
+        "progress": 100,
+        "status": status,
+        "response": response_text,
+        "file_attachements": list()
+    }
+
+    results[id] = response
+
+    return jsonify(response), 201
+
+@app.route("/v1/function-foo-sync/<string:id>", methods=["GET"])
+def function_foo_sync_status(id):
+    response = results[id]
+
+    return jsonify(response), 201
+
+@app.route("/v1/health-check", methods=["GET"])
+def health_check():
+    return {
+        "status": "ok"
+    }
