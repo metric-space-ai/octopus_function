785053bb4a24f28142703d818919b5f6a7157481
Michal Piotrowski <asyncfn.com@gmail.com>
2023-08-16 07:03:44 +0000

    #14 improvements v2 p1
     #14 improvements v2 p2
    
     #14 improvements v2 p3
    
     #14 improvements v2 p4
    
     #14 improvements v2 p5
    
     #14 improvements v2 p6
    
     #14 improvements v2 p7
    
     #14 improvements v2 p8
    
     #14 improvements v2 p9
    
     #14 improvements v2 p10
    
     #14 improvements v2 p11
    
     #14 improvements v2 p12
    
     #14 improvements v2 p13
    
     #14 improvements v2 p14
    
     #14 improvements v2 p15
    
     #14 improvements v2 p16
    
     #14 improvements v2 p17
    
     #14 improvements v2 p18

--- octopus_function_template.py
+++ octopus_function_template.py
@@ -1,35 +1,21 @@
 import base64, json, os, uuid
+from datetime import datetime, timedelta
 from flask import Flask, jsonify, request
-
-import utils
 
 app = Flask(__name__)
 
 ### Configuration section
 config = '''{
     "device_map": {
-       "max_memory": {"0": "10GiB", "1": "10GiB", "cpu": "30GiB"}
+        "device": "cpu",
+        "max_memory": {"0": "10GiB", "1": "10GiB", "cpu": "30GiB"}
     },
     "model_setup": {
-        "file": "model.dat"
+        "file": "model.dat",
+        "model_call_name": "model3B",
+        "model_real_name": "model/model-3B"
     },
     "functions": [
-        {
-            "name": "function_bar_async",
-            "url_part": "function-bar-async",
-            "description": "Asynchronous communication test function",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "value1": {
-                        "type": "string",
-                        "description": "First value"
-                    },
-                    "value2": { "type": "string", "enum": ["abc", "def"], "description": "Second value" }
-                },
-                "required": ["value1", "value2"]
-            }
-        },
         {
             "name": "function_foo_sync",
             "url_part": "function-foo-sync",
@@ -37,10 +23,7 @@ config = '''{
             "parameters": {
                 "type": "object",
                 "properties": {
-                    "value1": {
-                        "type": "string",
-                        "description": "First value"
-                    },
+                    "value1": { "type": "string", "description": "First value" },
                     "value2": { "type": "string", "enum": ["abc", "def"], "description": "Second value" }
                 },
                 "required": ["value1", "value2"]
@@ -52,204 +35,10 @@ config = json.loads(config)
 ### AI function section
 file = config["model_setup"]["file"]
 model = None
-results = {}
-
-def get_result(id):
-    return results[id]
-
-def set_result(id, response):
-    results[id] = response
-
-def setup():
-# Start editing here
-    f = open(file, "a")
-    f.write("Lets create some model file")
-    f.close()
-# Finish editing here
-
-def setup_condition() -> bool:
-# Start editing here
-    if not os.path.isfile(file):
-        return True
-# Finish editing here
-    return False
-
-# Please load all models to memory here
-def warmup():
-# Start editing here
-    global model
-    if model == None:
-        model = True
-# Finish editing here
-
-def warmup_condition() -> bool:
-# Start editing here
-    if model == None:
-        return True
-# Finish editing here
-    return False
-
-### AI service section
-@app.route("/v1/{url_part}/setup".format(url_part = config["functions"][0]["url_part"]), methods=["GET"])
-def function_bar_async_setup_status():
-    if setup_condition():
-        return {
-            "setup": "NotPerformed"
-        }, 200
-
-    return {
-        "setup": "Performed"
-    }, 200
-
-@app.route("/v1/{url_part}/setup".format(url_part = config["functions"][0]["url_part"]), methods=["POST"])
-def function_bar_async_setup():
-    data = request.json
-    force_setup = data.get("force_setup", False)
-
-    if setup_condition() or force_setup:
-        setup()
-
-    return {
-        "setup": "Performed"
-    }, 201
-
-@app.route("/v1/{url_part}/warmup".format(url_part = config["functions"][0]["url_part"]), methods=["GET"])
-def function_bar_async_warmup_status():
-    if warmup_condition():
-        return {
-            "warmup": "NotPerformed"
-        }, 200
-
-    return {
-        "warmup": "Performed"
-    }, 200
-
-@app.route("/v1/{url_part}/warmup".format(url_part = config["functions"][0]["url_part"]), methods=["POST"])
-def function_bar_async_warmup():
-    warmup()
-
-    return {
-        "warmup": "Performed"
-    }, 201
 
 @app.route("/v1/{url_part}".format(url_part = config["functions"][0]["url_part"]), methods=["POST"])
-def function_bar_async():
-# Start editing here
-    if setup_condition():
-        setup()
-    if warmup_condition():
-        warmup()
-
-    data = request.json
-    device_map = data.get("device_map", "")
-    value1 = data.get("value1", "")
-    value2 = data.get("value2", "")
-
-    id = str(uuid.uuid4())
-    status = "Initial"
-    response_text = "Some async response text " + value1 + " " + value2
-    estimated_response_at = utils.get_estimated_response_at(5)
-
-    response = {
-        "id": id,
-        "estimated_response_at": estimated_response_at,
-        "progress": 0,
-        "status": status,
-        "response": response_text,
-        "file_attachements": list()
-    }
-
-    set_result(id, response)
-
-    return jsonify(response), 201
-# Finish editing here
-
-@app.route("/v1/{url_part}/<string:id>".format(url_part = config["functions"][0]["url_part"]), methods=["GET"])
-def function_bar_async_status(id):
-# Start editing here
-    response = get_result(id)
-
-    estimated_response_at = utils.get_estimated_response_at(5)
-
-    if response["progress"] < 25:
-        response["estimated_response_at"] = estimated_response_at
-        response["progress"] = 25
-        response["status"] = "Processing"
-    elif response["progress"] < 50:
-        response["estimated_response_at"] = estimated_response_at
-        response["progress"] = 50
-    elif response["progress"] < 75:
-        response["estimated_response_at"] = estimated_response_at
-        response["progress"] = 75
-    elif response["progress"] < 100:
-        response["estimated_response_at"] = estimated_response_at
-        response["progress"] = 100
-        response["status"] = "Processed"
-        content = str(base64.b64encode(b"test of generated content"))
-        file_attachement = {
-            "content": content,
-            "file_name": "test.txt",
-            "media_type": "text/plain",
-        }
-        file_attachements = list()
-        file_attachements.append(file_attachement)
-        response["file_attachements"] = file_attachements
-
-    set_result(id, response)
-
-    return jsonify(response), 200
-# Finish editing here
-
-@app.route("/v1/{url_part}/setup".format(url_part = config["functions"][1]["url_part"]), methods=["GET"])
-def function_foo_sync_setup_status():
-    if setup_condition():
-        return {
-            "setup": "NotPerformed"
-        }, 200
-
-    return {
-        "setup": "Performed"
-    }, 200
-
-@app.route("/v1/{url_part}/setup".format(url_part = config["functions"][1]["url_part"]), methods=["POST"])
-def function_foo_sync_setup():
-    data = request.json
-    force_setup = data.get("force_setup", False)
-
-    if setup_condition() or force_setup:
-        setup()
-
-    return {
-        "setup": "Performed"
-    }, 201
-
-@app.route("/v1/{url_part}/warmup".format(url_part = config["functions"][1]["url_part"]), methods=["GET"])
-def function_foo_sync_warmup_status():
-    if warmup_condition():
-        return {
-            "warmup": "NotPerformed"
-        }, 200
-
-    return {
-        "warmup": "Performed"
-    }, 200
-
-@app.route("/v1/{url_part}/warmup".format(url_part = config["functions"][1]["url_part"]), methods=["POST"])
-def function_foo_sync_warmup():
-    warmup()
-
-    return {
-        "warmup": "Performed"
-    }, 201
-
-@app.route("/v1/{url_part}".format(url_part = config["functions"][1]["url_part"]), methods=["POST"])
 def function_foo_sync():
 # Start editing here
-    if setup_condition():
-        setup()
-    if warmup_condition():
-        warmup()
-
     data = request.json
     device_map = data.get("device_map", "")
     value1 = data.get("value1", "")
@@ -258,32 +47,42 @@ def function_foo_sync():
     id = str(uuid.uuid4())
     status = "Processed"
     response_text = "Some sync response text " + value1 + " " + value2
-    estimated_response_at = utils.get_estimated_response_at(5)
+
+    content = str(base64.b64encode(b"test of generated content"))
+    file_attachement = {
+        "content": content,
+        "file_name": "test.txt",
+        "media_type": "text/plain",
+    }
+    file_attachements = list()
+    file_attachements.append(file_attachement)
 
     response = {
         "id": id,
-        "estimated_response_at": estimated_response_at,
         "progress": 100,
         "status": status,
         "response": response_text,
-        "file_attachements": list()
+        "file_attachements": file_attachements
     }
-
-    set_result(id, response)
 
     return jsonify(response), 201
 # Finish editing here
 
-@app.route("/v1/{url_part}/<string:id>".format(url_part = config["functions"][1]["url_part"]), methods=["GET"])
-def function_foo_sync_status(id):
+@app.route("/v1/setup", methods=["POST"])
+def setup():
 # Start editing here
-    response = get_result(id)
+    global model
+    data = request.json
+    force_setup = data.get("force_setup", False)
 
-    return jsonify(response), 200
+    if not os.path.isfile(file) or force_setup:
+        f = open(file, "a")
+        f.write("Lets create some model file")
+        f.close()
+
+    if model == None:
+        model = True
 # Finish editing here
-
-@app.route("/v1/health-check", methods=["GET"])
-def health_check():
     return {
-        "status": "Ok"
-    }, 200
+        "setup": "Performed"
+    }, 201
--- utils.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, timedelta
-
-def get_estimated_response_at(seconds: int) -> str:
-    estimated_response_at = str(datetime.now() + timedelta(seconds=seconds))
-    estimated_response_at = estimated_response_at + "Z"
-    estimated_response_at = estimated_response_at.replace(" ", "T")
-
-    return estimated_response_at
