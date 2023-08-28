01bae99497b753af5adf3fbc3d7281e1752a0cb5
Michal Piotrowski <asyncfn.com@gmail.com>
2023-08-28 12:09:14 +0000

    #19 data privacy p1
     #19 data privacy p2
    
     #19 data privacy p3
    
     #19 data privacy p4

--- start.sh
+++ start.sh
@@ -2,3 +2,4 @@
 
 nohup flask --app octopus_function_translator run -h 0.0.0.0 -p 5001 2>&1 &
 nohup flask --app octopus_plugin_blender_orbit_camera run -h 0.0.0.0 -p 5002 2>&1 &
+nohup flask --app octopus_DataPrivacyComplianceCheck run -h 0.0.0.0 -p 5004 2>&1 &
