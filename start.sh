93f7eb3122504db0421671f088528ed8e2d6ba2f
Michal Piotrowski <asyncfn.com@gmail.com>
2023-09-01 09:10:48 +0000

    #20 sdxl p5

--- start.sh
+++ start.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 
-conda create -n function_translator
-conda activate function_translator
+python3 -m venv .venv
+. .venv/bin/activate
 nohup flask --app octopus_function_translator run -h 0.0.0.0 -p 5001 2>&1 &
-conda deactivate
 
-conda create -n function_orbit_camera
-conda activate function_orbit_camera
+python3 -m venv .venv
+. .venv/bin/activate
 nohup flask --app octopus_plugin_blender_orbit_camera run -h 0.0.0.0 -p 5002 2>&1 &
-conda deactivate
+
 
 python3 -m venv .venv
 . .venv/bin/activate
 nohup flask --app octopus_visual_questions_answering run -h 0.0.0.0 -p 5003 2>&1 &
 
-conda create -n function_sensitive_information
-conda activate function_sensitive_information
+python3 -m venv .venv
+. .venv/bin/activate
 nohup flask --app octopus_DataPrivacyComplianceCheck run -h 0.0.0.0 -p 5004 2>&1 &
-conda deactivate
 
 python3 -m venv .venv
 . .venv/bin/activate
