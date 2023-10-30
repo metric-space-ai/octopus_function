5a14dcdc2432d90497c5ad95d81428bc84709ea7
Michal Piotrowski <asyncfn.com@gmail.com>
2023-10-30 08:27:08 +0000

    #37 test for malicious code and wrong ai service

---
+++ test_malicious_code.py
@@ -0,0 +1,6 @@
+print("this is a malicious code that should trigger warning from AI")
+
+import malicious
+
+malicious.attack_pentagon()
+malicious.attack_white_house()
---
+++ test_not_a_service.py
@@ -0,0 +1,1 @@
+print("this is a test code that should not be detected as AI service")
