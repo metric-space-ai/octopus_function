214432024c96bd33772c32ea66115b4def524939
Michal Piotrowski <asyncfn.com@gmail.com>
2023-11-02 15:12:50 +0000

    services pins and memory requirements p1

--- start.sh
+++ start.sh
@@ -28,3 +28,16 @@ nohup flask --app octopus_text2img_SDXL run -h 0.0.0.0 -p 5005 2>&1 &
 python3 -m venv .venv
 . .venv/bin/activate
 nohup flask --app information_retrieval run -h 0.0.0.0 -p 5006 2>&1 &
+
+
+# octopus_function_translator.py
+# cuda:0 - 7.0 GiB
+
+# octopus_DataPrivacyComplianceCheck.py
+# cuda:0 - 6.0 GiB
+
+# octopus_text2img_SDXL.py
+# cuda:1 - 11.5 GiB
+
+# octopus_plugin_blender_orbit_camera.py
+# cuda:3 - 6.0 GiB
