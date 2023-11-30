8220ce540ef9abc3173747b3ae504c9ae656eb92
Michal Piotrowski <asyncfn.com@gmail.com>
2023-11-30 14:03:36 +0000

    #43 anonymization service update p4

--- start.sh
+++ start.sh
@@ -42,5 +42,8 @@ nohup flask --app octopus_anonymization run -h 0.0.0.0 -p 5007 2>&1 &
 # octopus_text2img_SDXL.py
 # cuda:1 - 11.5 GiB
 
+# octopus_anonymization.py
+# cuda:2 - 12.0 GiB
+
 # octopus_plugin_blender_orbit_camera.py
-# cuda:3 - 6.0 GiB
+# cuda:3 - 1.8 GiB
